7ac02d06d46cafbb2febe548ff018d73d05bd4da
mcmeschine <mmc@corelia.ai>
2023-03-22 15:20:58 +0100

    Finished Gatsby Tutorial Part 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,9 @@
 module.exports = {
   siteMetadata: {
     title: `My First Gatsby Site`,
-    siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+  ],
 }
